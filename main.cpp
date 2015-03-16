18b0c571fe9eb78118f92537eb4cfbdf9c0cfbbd
M42 <mromang08@gmail.com>
2015-03-16 20:09:18 +0100

    Completes the first exercise

--- main.cpp
+++ main.cpp
@@ -4,12 +4,14 @@
 
 using namespace std;
 
-const int num_items = 50;
-const int tam_vect  = 200;
+const int num_items = 10;
+const int tam_vect  = 5;
 int buffer[tam_vect];
 int pos_vect = 0;
 sem_t puede_leer;
+sem_t puede_escr;
 sem_t mutex;
+
 
 int producir_dato();
 void consumir_dato(int dato);
@@ -21,6 +23,7 @@ int main() {
     pthread_t hebra_cons, hebra_prod;
 
     sem_init(&puede_leer, 0, 0);
+    sem_init(&puede_escr, 0, tam_vect);
     sem_init(&mutex, 0, 1);
 
     pthread_create(&hebra_cons, NULL, consumidor, NULL);
@@ -39,18 +42,19 @@ void consumir_dato(int dato) {
 }
 
 void* productor(void*) {
-   for (unsigned i=0; i<num_items; i++) {
+    for (unsigned i=0; i<num_items; i++) {
         int dato = producir_dato();
-
+        
         // Escribir dato al vector
+        sem_wait(&puede_escr);
         sem_wait(&mutex);
+
         buffer[pos_vect] = dato;
         cerr << "Escrito: " << dato << "  En pos: " << pos_vect << endl;
         pos_vect++;
 
         sem_post(&mutex);
         sem_post(&puede_leer);
-
     }
     return NULL;
 }
@@ -62,10 +66,13 @@ void* consumidor(void*) {
         // Leer dato desde vector
         sem_wait(&puede_leer);
         sem_wait(&mutex);
+
         pos_vect--;
         dato = buffer[pos_vect];
         cerr << "Leido: " << dato << "  En pos: " << pos_vect << endl;
+
         sem_post(&mutex);
+        sem_post(&puede_escr);
 
         consumir_dato(dato);
 
