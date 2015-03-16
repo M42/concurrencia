ca8016b5925eea238b25ddc0e9483b635cacc167
M42 <mromang08@gmail.com>
2015-03-16 19:49:22 +0100

    Prepara el programa

--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 #include <pthread.h>
+#include <semaphore.h>
 
 using namespace std;
 
-const int num_items = 100;
-const int tam_vect  = 10;
+const int num_items = 50;
+const int tam_vect  = 200;
+int buffer[tam_vect];
+int pos_vect = 0;
+sem_t puede_leer;
+sem_t mutex;
 
 int producir_dato();
-void consumir_dato();
+void consumir_dato(int dato);
 void* productor(void*);
+void* consumidor(void*);
 
 
 int main() {
+    pthread_t hebra_cons, hebra_prod;
+
+    sem_init(&puede_leer, 0, 0);
+    sem_init(&mutex, 0, 1);
+
+    pthread_create(&hebra_cons, NULL, consumidor, NULL);
+    pthread_create(&hebra_prod, NULL, productor, NULL);
+    pthread_exit(NULL);
 }
 
 
@@ -25,9 +39,18 @@ void consumir_dato(int dato) {
 }
 
 void* productor(void*) {
-    for (unsigned i=0; i<num_items; i++) {
+   for (unsigned i=0; i<num_items; i++) {
         int dato = producir_dato();
-        // Leer dato desde vector
+
+        // Escribir dato al vector
+        sem_wait(&mutex);
+        buffer[pos_vect] = dato;
+        cerr << "Escrito: " << dato << "  En pos: " << pos_vect << endl;
+        pos_vect++;
+
+        sem_post(&mutex);
+        sem_post(&puede_leer);
+
     }
     return NULL;
 }
@@ -35,7 +58,16 @@ void* productor(void*) {
 void* consumidor(void*) {
     for (unsigned i=0; i<num_items; i++) {
         int dato;
+
         // Leer dato desde vector
+        sem_wait(&puede_leer);
+        sem_wait(&mutex);
+        pos_vect--;
+        dato = buffer[pos_vect];
+        cerr << "Leido: " << dato << "  En pos: " << pos_vect << endl;
+        sem_post(&mutex);
+
         consumir_dato(dato);
+
     }
 }
