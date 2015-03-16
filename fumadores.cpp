4995d4e8d0e9d14ed3fc380788662d007bcdb456
M42 <mromang08@gmail.com>
2015-03-16 20:56:33 +0100

    Completes the code template

--- fumadores.cpp
+++ fumadores.cpp
@@ -4,15 +4,59 @@
 
 
 void fumar();
-
+void* consumir(void*);
+void* repartir(void*);
+sem_t mostrador;
+sem_t papel;
+sem_t cerll;
+sem_t tabac;
 
 int main() {
     // Random seed
     srand(time(NULL)); 
 
-    
+    // Hebras
+    pthread_t fumador_sinpapel;
+    pthread_t fumador_sincerll;
+    pthread_t fumador_sintabac;
+    pthread_t estanquero;
+
+    // Semáforos
+    sem_init(&mostrador, 0, 1);
+    sem_init(&papel, 0, 0);
+    sem_init(&cerll, 0, 0);
+    sem_init(&tabac, 0, 0);
+
+    // Inicializa hebras
 }
 
+void* repartir(void*) {
+    while (true) {
+        int ingrediente = rand() % 3;
+
+        sem_wait(&mostrador);
+
+        switch (ingrediente) {
+        case 0: sem_post(&papel); break;
+        case 1: sem_post(&cerll); break;
+        case 2: sem_post(&tabac); break;
+        }
+    }
+}
+
+void* consumir(void* objeto) {
+    sem_t& ingrediente;
+
+    switch (int(objeto)) {
+    case 0: ingrediente = papel; break;
+    case 1: ingrediente = cerll; break;
+    case 2: ingrediente = tabac; break;
+    }
+
+    sem_wait(ingrediente);
+    sem_post(&mostrador);
+    fumar();
+}
 
 void fumar() {
     // Fuma un número aleatorio de milisegundos
